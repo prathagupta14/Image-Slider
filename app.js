3afd994d967d36a6f7fb086e8e832778167a41d9
Pratha Gupta <69582630+prathagupta14@users.noreply.github.com>
2021-10-20 15:13:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const sliderImages = document.querySelector(".images");
+let counter = 1;
+
+function nextSlide() {
+    sliderImages.style.transform = `translateX(${-285 * counter}px)`;
+    console.log('hi');
+    counter++;
+    sliderImages.style.transition = 'transform 1.5s ease-in-out';
+
+    if(counter === 3){
+        counter = 0;
+    }
+}
+
+setInterval(nextSlide, 2000);
