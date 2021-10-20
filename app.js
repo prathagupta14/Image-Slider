38d8330d459bab6fc5df4d4f705518bb5231e7ba
Pratha Gupta <69582630+prathagupta14@users.noreply.github.com>
2021-10-20 15:18:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const sliderImages = document.querySelector(".images");
 let counter = 1;
 
 function nextSlide() {
-    sliderImages.style.transform = `translateX(${-285 * counter}px)`;
+    sliderImages.style.transform = `translateX(${-20 * counter}%)`;
     console.log('hi');
     counter++;
     sliderImages.style.transition = 'transform 1.5s ease-in-out';
